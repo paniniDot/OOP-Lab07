20360d83a44c57db07a4c4e534e0a7f5baa03e20
Mattia Panni <mattia.panni@studio.unibo.it>
2021-11-13 17:41:34 +0100

    Hypothetical implementation of OneListAcceptable

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -1,5 +1,39 @@
 package it.unibo.oop.lab.nesting2;
 
-public class OneListAcceptable {
+import java.util.ArrayList;
+import java.util.List;
+
+public class OneListAcceptable<T> implements Acceptable<T> {
+	
+	private final List<T> inputList;
+	private final List<T> myList;
+	
+	public OneListAcceptable(List<T> list) {
+		this.inputList = list;
+		this.myList = new ArrayList<>();
+	}
+
+	@Override
+	public Acceptor<T> acceptor() {
+		return new Acceptor<T>() {
+
+			@Override
+			public void accept(T newElement) throws ElementNotAcceptedException {
+				if (inputList.contains(newElement)) {
+					myList.add(newElement);
+				} else {
+					throw new ElementNotAcceptedException(newElement);
+				}
+			}
+
+			@Override
+			public void end() throws EndNotAcceptedException {
+				if(inputList.size() > myList.size()) {
+					throw new EndNotAcceptedException();
+				}
+			}
+			
+		};
+	}
 
 }
