7b9c94a854772cbf479892e0fb5280c329046b5a
Mattia Panni <mattia.panni@studio.unibo.it>
2021-11-13 14:15:56 +0100

    Create sport enum class

--- src/it/unibo/oop/lab/enum1/Sport.java
+++ src/it/unibo/oop/lab/enum1/Sport.java
@@ -10,8 +10,21 @@ package it.unibo.oop.lab.enum1;
  * 
  */
 public enum Sport {
-    /*
-     * declare the following sports: - basket - soccer - tennis - bike - F1 -
-     * motogp - volley
-     */
+   
+	BASKET("Basketball"), SOCCER("Soccer"), TENNIS("Tennis"), BIKE("Bike"), F1("Formula 1"),
+	MOTOGP("Moto GP"), VOLLEY("Volleyball");
+	
+	private final String actualName;
+	
+	private Sport(String name) {
+		this.actualName = name;
+	}
+	
+	@Override
+	public String toString() {
+		return this.actualName;
+	}
+	
+	
 }
+
