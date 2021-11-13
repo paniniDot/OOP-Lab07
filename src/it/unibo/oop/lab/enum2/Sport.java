e38e1fcf4352e48621d8e384c6ebfda158838f2a
Mattia Panni <mattia.panni@studio.unibo.it>
2021-11-13 15:42:36 +0100

    Create sport enum

--- src/it/unibo/oop/lab/enum2/Sport.java
+++ src/it/unibo/oop/lab/enum2/Sport.java
@@ -3,59 +3,64 @@
  */
 package it.unibo.oop.lab.enum2;
 
-/**
- * Represents an enumeration for declaring sports.
- * 
- * 1) You must add a field keeping track of the number of members each team is
- * composed of (1 for individual sports)
- * 
- * 2) A second field will keep track of the name of the sport.
- * 
- * 3) A third field, of type Place, will allow to define if the sport is
- * practiced indoor or outdoor
- * 
- */
 public enum Sport {
-
-    /*
-     * TODO
-     * 
-     * Declare the following sports:
-     * 
-     * - basket
-     * 
-     * - volley
-     * 
-     * - tennis
-     * 
-     * - bike
-     * 
-     * - F1
-     * 
-     * - motogp
-     * 
-     * - soccer
-     * 
-     */
-
-    /*
-     * TODO
-     * 
-     * [FIELDS]
-     * 
-     * Declare required fields
-     */
-
-    /*
-     * TODO
-     * 
-     * [CONSTRUCTOR]
-     * 
-     * Define a constructor like this:
-     * 
-     * - Sport(final Place place, final int noTeamMembers, final String actualName)
-     */
-
+	
+	BASKET("Basket", 10, Place.INDOOR), VOLLEY("Volley", 12, Place.INDOOR), TENNIS("Tennis", 2, Place.OUTDOOR),
+	BIKE("Bike", 1, Place.OUTDOOR), F1("Formula 1", 1, Place.OUTDOOR), MOTOGP("Moto GP", 1, Place.OUTDOOR), SOCCER("Soccer", 11, Place.OUTDOOR);
+     
+	private final static int INDIVIDUAL = 1; 
+	
+	private final String name;
+	private final int nMembers;
+	private final Place place;
+	
+	/**
+	 * 
+	 * @param name
+	 * 		actual name of the sport 
+	 * @param nMembers
+	 * 		the number of members each team is composed of (1 for individual sports)
+	 * @param place
+	 * 		place where sport is practiced 
+	 */
+	private Sport(final String name, final int nMembers, final Place place) {
+		this.name = name;
+		this.nMembers = nMembers;
+		this.place = place;
+	}
+	
+	/**
+	 * 
+	 * @return true if is an individual sport
+	 */
+	public boolean isIndividualSport() {
+		return this.nMembers == Sport.INDIVIDUAL;
+	}
+	
+	/**
+	 * 
+	 * @return true if is an indoor sport
+	 */
+	public boolean isIndoorSport() {
+		return this.place == Place.INDOOR;
+	}
+	
+	/**
+	 * 
+	 * @return where the sport is played
+	 */
+	public Place getPlace() {
+		return this.place;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		return this.name + "practiced " + this.place + "in " + this.nMembers + " players";
+	}
+	
     /*
      * TODO
      * 
