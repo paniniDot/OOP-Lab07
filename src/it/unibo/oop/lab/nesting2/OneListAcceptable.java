f6deca4a67c8b0af12bc839505a056c831b5e3fc
Mattia Panni <mattia.panni@studio.unibo.it>
2021-11-13 17:44:09 +0100

    Update javadoc

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -5,18 +5,36 @@ import java.util.List;
 
 public class OneListAcceptable<T> implements Acceptable<T> {
 	
+	/**
+	 *  list to be accepted
+	 */
 	private final List<T> inputList;
+	
+	/**
+	 *  internal list
+	 */
 	private final List<T> myList;
 	
+	/**
+	 * 
+	 * @param list
+	 * 		list to be iterated
+	 */
 	public OneListAcceptable(List<T> list) {
 		this.inputList = list;
 		this.myList = new ArrayList<>();
 	}
-
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Acceptor<T> acceptor() {
 		return new Acceptor<T>() {
 
+			/**
+			 * {@inheritDoc}
+			 */
 			@Override
 			public void accept(T newElement) throws ElementNotAcceptedException {
 				if (inputList.contains(newElement)) {
@@ -25,7 +43,10 @@ public class OneListAcceptable<T> implements Acceptable<T> {
 					throw new ElementNotAcceptedException(newElement);
 				}
 			}
-
+			
+			/**
+			 * {@inheritDoc}
+			 */
 			@Override
 			public void end() throws EndNotAcceptedException {
 				if(inputList.size() > myList.size()) {
