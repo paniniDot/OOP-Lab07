c77d7c7732049df4c708b5134b6d3284983c7848
Mattia Panni <mattia.panni@studio.unibo.it>
2021-11-13 14:17:28 +0100

    Update javadoc

--- src/it/unibo/oop/lab/enum1/Sport.java
+++ src/it/unibo/oop/lab/enum1/Sport.java
@@ -14,12 +14,24 @@ public enum Sport {
 	BASKET("Basketball"), SOCCER("Soccer"), TENNIS("Tennis"), BIKE("Bike"), F1("Formula 1"),
 	MOTOGP("Moto GP"), VOLLEY("Volleyball");
 	
+	/**
+	 * Well formatted name of enum
+	 */
 	private final String actualName;
 	
+	/**
+	 * 
+	 * @param name
+	 * 		actual name of the sport 
+	 */
 	private Sport(String name) {
 		this.actualName = name;
 	}
 	
+	/**
+	 * 
+	 * @return actual name of sport
+	 */
 	@Override
 	public String toString() {
 		return this.actualName;
